7f347e0b5341f816ebeb2786585f2a03695af627
gabrielhuff47 <gabriel_quintanahuff@hotmail.com>
2024-04-30 21:54:01 -0300

    última alteração no desafio da trilha Java básico

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        contaBanco();
     }
+
+    public static void contaBanco() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o numero da conta");
+          int numero = leitor.nextInt();
+         
+        
+        System.out.println("Digite o numero da agencia");
+         String agencia = leitor.next();
+        
+        System.out.println("Digite o nome do cliente");
+          String nomeCliente = leitor.next();
+
+        System.out.println("Digite o Saldo:");
+         double saldo = leitor.nextDouble();
+
+        imprimir(numero, agencia, nomeCliente, saldo);
+    }
+
+    public static void imprimir(int numero, String agencia, String nomeCliente, Double saldo) {
+   
+      System.out.println("Olá " +nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia  + " conta " + numero + " e seu saldo " + saldo + " já está disponivel para saque.");
+      }
 }
